f8f18de1a1960035309248a1529dee84b6c0bd6c
Eylaine <46253285@163.com>
2018-06-01 20:22:18 +0800

    add

---
+++ template_engine/demo.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python
+# _*_ coding: utf-8 _*_
+
+# @Time    : 2018/6/1
+# @Author  : zhonglin.zhang
+
+from jinja2 import Template
+
+template = Template()
+template.render(name='World')
