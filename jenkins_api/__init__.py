08e87b3578078cf71b5194e04b799149cb81e47e
aikucun <aikucun@eylaine.local>
2019-07-26 20:27:33 +0800

    add

---
+++ jenkins_api/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-07-26 20:23
+# @Author  : Eylaine
+# @File    : __init__.py
--- orm/__init__.py
+++
@@ -1,5 +0,0 @@
-# !/usr/bin/python
-# _*_ coding: utf-8 _*_
-
-# @Time    : 2018/6/5
-# @Author  : zhonglin.zhang
